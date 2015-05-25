061ef0a8f686e5b801cae6bf6489eff92bd1decd
okada.yosuke <nakayama.nobuo@nichiwa-system.co.jp>
2015-05-25 19:36:23 +0900

    ソートプログラム、改正中。 現在、名前と数値が連動するようにはなったが、 IDが連動していないので、次回その部分を訂正。

---
+++ app/src/main/java/jp/co/nichiwa_system/okadayosuke/myapplication/ad.java
@@ -0,0 +1,25 @@
+package jp.co.nichiwa_system.okadayosuke.myapplication;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ad extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ad, container, false);
+    }
+
+}
